618e41428fc53c8d7723b764a06a3116e2f9afb8
Raisa Tarasova <raisatarasova@MacBook-Pro-Raisa.local>
2020-04-17 16:16:58 +0300

    Updates

---
+++ precache-manifest.e58480329124c73a9fc0a69dfca059d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8953cce69ecfbe80821633aafe1f45e9",
+    "url": "/sqa-mate-admin/index.html"
+  },
+  {
+    "revision": "651bc9559db54cb1d49c",
+    "url": "/sqa-mate-admin/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "a262228ef495aec8a008",
+    "url": "/sqa-mate-admin/static/css/main.56657f27.chunk.css"
+  },
+  {
+    "revision": "651bc9559db54cb1d49c",
+    "url": "/sqa-mate-admin/static/js/2.0ec94eb1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sqa-mate-admin/static/js/2.0ec94eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a262228ef495aec8a008",
+    "url": "/sqa-mate-admin/static/js/main.82e0d0a7.chunk.js"
+  },
+  {
+    "revision": "ed15f688ba60066d2363",
+    "url": "/sqa-mate-admin/static/js/runtime-main.8151c172.js"
+  }
+]);
